3297190f84915385f262d2a5e2488f04b21a23cf
Marcelo Maza <xydelta04@gmail.com>
2022-01-14 22:02:54 -0500

    added cross-reference solution for student/course

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: String,
   googleid: String,
-  course: [Number,],
+  course: [String],
 });
 
 // compile model from schema
