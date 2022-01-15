0f78f17f751d58d4c7e95d53d50387bc93b1b64b
maxzetina <74638275+maxzetina@users.noreply.github.com>
2022-01-15 14:49:38 -0500

    updated overview class cards

--- client/src/components/Class.js
+++ client/src/components/Class.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import ClassDetails from './ClassDetails.js'
 
 import './Class.css'
 
 const Class = (props) => {
-    let style = [{backgroundColor: props.color}, {borderBottomColor: props.color}]
+    let style = [{backgroundColor: props.color}, {borderColor: props.color}]
+
+    const [details, setDetails] = useState(false)
+
+    const handleClick = () => {
+        details ? (
+            setDetails(false) 
+        ) : (setDetails(true))
+    }
 
     return (
-        <div className="class-container">
-            <div className="upper-half" style={style[0]}>
-                <h1 className="class-name">{props.name}</h1>
+        details ? (
+            <div>
+                <ClassDetails name={props.name} color={props.color} onClick={handleClick} />
+            </div> 
+            ) : (
+            <div className="class-container">
+                <div className="upper-half" style={style[0]}>
+                    <h1 className="class-name">
+                        {props.name} <span style={{color: 'white'}} onClick={handleClick}>+</span>
+                    </h1>
+                </div>
+                <div className="bottom-half" style={style[1]}>
+                    <p> <span className="head">Due: </span> {props.assignment.dueDate.toString()}: {props.assignment.name}</p>
+                    <p> <span className="head">Grade: </span> {props.grade}</p>
+                </div>
             </div>
-            <div className="bottom-half" style={style[1]}>
-                <p>Due {props.assignment.dueDate.toString()}: {props.assignment.name}</p>
-                <p>Grade: {props.grade}</p>
-            </div>
-        </div>
-    )
+            )
+        )
 }
 
 export default Class
