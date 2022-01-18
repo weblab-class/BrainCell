760c3a0e3315a8bf14834adc893f2b3ad547d32d
maxzetina <74638275+maxzetina@users.noreply.github.com>
2022-01-18 00:58:07 -0500

    updated login page

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -10,7 +10,7 @@ const GOOGLE_CLIENT_ID = "40738148267-lth98utc4vhae0oqu44dtntfu5ldhk82.apps.goog
 const LoginPage = (props) => {
 
     return (
-    <div style={{backgroundImage: `url(${img})`, height: window.innerHeight, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+    <div style={{backgroundImage: `url(${img})`, height: window.innerHeight, display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundSize: 'cover'}}>
         <div>
             <div className="logo">
                 LOGO
