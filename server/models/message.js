2a3660905fd54575bf9d975b659aae8c1c1b080e
Marcelo Maza <xydelta04@gmail.com>
2022-01-17 23:06:35 -0500

    Revert "Added message schema"
    This reverts commit 8173e3588a0f470f5b419f2105fd04c45c4ce67c.

--- server/models/message.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const messageSchema = new mongoose.Schema({
-    timestamp: {type: Date, default: Date.now},
-    content: String,
-    answerTo : {type: String, default: ""},
-});
-
-module.exports = mongoose.model("message", messageSchema)
