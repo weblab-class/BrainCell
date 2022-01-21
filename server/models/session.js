466538260cabd33446bc06747671e6c6f7bd5e75
Marcelo Maza <xydelta04@gmail.com>
2022-01-21 12:25:09 -0500

    added session.js, removed assignment.js

---
+++ server/models/session.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+//define a message schema for the database
+const MessageSchema = new mongoose.Schema({
+    timestamp: { type: Date, default: Date.now },
+    content: String,
+    answerTo: "mixed",
+});
+
+const sessionSchema = new mongoose.Schema({
+    slides: [],
+    courseId: String,
+    messages: [MessageSchema],
+})
+// compile model from schema
+module.exports = mongoose.model("session", sessionSchema);
